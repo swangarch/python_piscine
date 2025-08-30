d31b77302be5416d259fcbf22255d245ec5c74a6
Shuai Wang <shuwang@f6r3s14.clusters.42paris.fr>
2025-08-30 16:29:10 +0200

    fixed flake8 for ex05

--- 01/ex05/load_image.py
+++ 01/ex05/load_image.py
@@ -7,33 +7,32 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def ft_show_img(imgArr:array, title:str) -> None:
-	"""Function to show image, takes image array and title as input."""
+def ft_show_img(imgArr: array, title: str) -> None:
+    """Function to show image, takes image array and title as input."""
 
-	plt.title(title)
-	if len(imgArr.shape) == 3:
-		plt.imshow(imgArr)
-	elif len(imgArr.shape) == 2:
-		plt.imshow(imgArr, cmap='grey')
-	else:
-		raise Exception("Wrong shape array.")
-	plt.show()
+    plt.title(title)
+    if len(imgArr.shape) == 3:
+        plt.imshow(imgArr.squeeze())
+    elif len(imgArr.shape) == 2:
+        plt.imshow(imgArr, cmap='grey')
+    else:
+        raise Exception("Wrong shape array.")
+    plt.show()
 
 
 def ft_load(path: str) -> array | None:
-	"""Function that loads an image with path, prints its format,
+    """Function that loads an image with path, prints its format,
     and its pixels content in RGB format. The function works with
     JPG and JPEG format."""
 
-	try:
-		img = Image.open(path)
-		
-		arr = np.array(img)
-		print("The shape of image is:", arr.shape)
-		print(arr)
-		ft_show_img(arr, "Figure VIII.1: Original")
-		return arr
+    try:
+        img = Image.open(path)
+        arr = np.array(img)
+        print("The shape of image is:", arr.shape)
+        print(arr)
+        ft_show_img(arr, "Figure VIII.1: Original")
+        return arr
 
-	except Exception as e:
-		print("Error load:", e)
-		sys.exit(1)
+    except Exception as e:
+        print("Error load:", e)
+        sys.exit(1)
--- 01/ex05/tester.py
+++ 01/ex05/tester.py
@@ -7,21 +7,21 @@ from pimp_image import ft_grey
 
 
 def main():
-	"""Main function to test ft_load, ft_invert, ft_red,
-	ft_green, ft_blue, ft_grey, display the original data and images."""
+    """Main function to test ft_load, ft_invert, ft_red,
+    ft_green, ft_blue, ft_grey, display the original data and images."""
 
-	try:
-		array = ft_load("landscape.jpg")
-		ft_invert(array)
-		ft_red(array)
-		ft_green(array)
-		ft_blue(array)
-		ft_grey(array)
+    try:
+        array = ft_load("landscape.jpg")
+        ft_invert(array)
+        ft_red(array)
+        ft_green(array)
+        ft_blue(array)
+        ft_grey(array)
 
-		print(ft_invert.__doc__)
-	except Exception as e:
-		print("Error:", e)
+        print(ft_invert.__doc__)
+    except Exception as e:
+        print("Error:", e)
 
 
 if __name__ == "__main__":
-	main()
+    main()
